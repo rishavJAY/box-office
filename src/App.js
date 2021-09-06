c1443977c896a765069532377dd4b1bed8a89c27
rishav <gupta.rishav512@gmail.com>
2021-09-06 22:58:54 +0530

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      hi
-    </div>
+    <Switch>
+    {/* exact is same as exact={true}, it is for matching exact path with path prop */}
+    {/* below switch and route is for visiting different pages at different urls */}
+      <Route exact path="/">
+        This is home page
+      </Route>
+
+      <Route exact path="/starred">
+        This is starred page
+      </Route>
+
+      <Route>
+        This is 404 page
+      </Route>
+    </Switch>
   );
 }
 
