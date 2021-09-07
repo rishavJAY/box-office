7e920ebcd6a80ffd72ea1ecffd6d3f144ad31cc1
rishav <gupta.rishav512@gmail.com>
2021-09-07 20:04:11 +0530

    added navbar and created page components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Navs from "./components/Navs"
+import Home from './pages/Home';
+import Starred from './pages/Starred';
 
 function App() {
   return (
-    <Switch>
-    {/* exact is same as exact={true}, it is for matching exact path with path prop */}
-    {/* below switch and route is for visiting different pages at different urls */}
-      <Route exact path="/">
-        This is home page
-      </Route>
+    <div>
+      <Navs />
+      <Switch>
+        {/* exact is same as exact={true}, it is for matching exact path with path prop */}
+        {/* below switch and route is for visiting different pages at different urls */}
+        <Route exact path="/">
+          <Home />
+        </Route>
 
-      <Route exact path="/starred">
-        This is starred page
-      </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
 
-      <Route>
-        This is 404 page
-      </Route>
-    </Switch>
+        <Route>
+          This is 404 page
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
