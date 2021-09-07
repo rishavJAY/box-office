68c52b1d4af680e795547e29472f04185b24fea6
rishav <gupta.rishav512@gmail.com>
2021-09-07 20:32:31 +0530

    added layout management and created title

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Navs from "./components/Navs"
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 
 function App() {
   return (
-    <div>
-      <Navs />
-      <Switch>
-        {/* exact is same as exact={true}, it is for matching exact path with path prop */}
-        {/* below switch and route is for visiting different pages at different urls */}
-        <Route exact path="/">
-          <Home />
-        </Route>
 
-        <Route exact path="/starred">
-          <Starred />
-        </Route>
+    <Switch>
+      {/* exact is same as exact={true}, it is for matching exact path with path prop */}
+      {/* below switch and route is for visiting different pages at different urls */}
+      <Route exact path="/">
+        <Home />
+      </Route>
 
-        <Route>
-          This is 404 page
-        </Route>
-      </Switch>
-    </div>
+      <Route exact path="/starred">
+        <Starred />
+      </Route>
+
+      <Route>
+        This is 404 page
+      </Route>
+    </Switch>
+
   );
 }
 
