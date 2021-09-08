773a4fd40a1a81e6746741091de1c3c02eeac703
rishav <gupta.rishav512@gmail.com>
2021-09-08 19:47:30 +0530

    added search option

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,9 +5,12 @@ import { apiGet } from '../misc/config';
 const Home = () => {
   const [input, setInput] = useState('');
   const [results, setResults] = useState(null);
-  
+  const [searchOption, setSearchOption] = useState('shows');
+
+  const isShowsSearch = searchOption === 'shows';
+
   const onSearch = () => {
-    apiGet(`/search/shows?q=${input}`).then(result => {
+    apiGet(`/search/${searchOption}?q=${input}`).then(result => {
       setResults(result);
     });
   };
@@ -22,7 +25,9 @@ const Home = () => {
     setInput(ev.target.value);
   };
 
- 
+  const onRadioChange = (ev) => {
+    setSearchOption(ev.target.value);
+  }
 
   const renderResults = () => {
     if (results && results.length === 0) {
@@ -30,11 +35,14 @@ const Home = () => {
     }
     if (results && results.length > 0) {
       return (
-        <div>
-          {results.map(item => (
+        results[0].show ? 
+          results.map(item => (
             <div key={item.show.id} > {item.show.name} </div>
-          ))}
-        </div>
+          ))
+         : 
+          results.map(item => (
+            <div key={item.person.id} > {item.person.name} </div>
+          ))
       );
     }
     return null;
@@ -44,7 +52,16 @@ const Home = () => {
     <MainPageLayout>
       <input type="text" placeholder="Search for something" onChange={onInputChange} onKeyDown={onKeyDown} value={input} />
 
-      
+      <div>
+        <label htmlFor="shows-search">
+          Shows
+          <input type="radio" checked={isShowsSearch} id="shows-search" value="shows" onChange={onRadioChange} />
+        </label>
+        <label htmlFor="shows-search">
+          Actors
+          <input type="radio" checked={!isShowsSearch} id="actors-search" value="people" onChange={onRadioChange} />
+        </label>
+      </div>
 
       <button type="button" onClick={onSearch}>Search</button>
       {renderResults()}
